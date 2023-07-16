8dc53afb609b1492386d7ba1cdaf6fc8ce87d471
MianFaizanAmir0053 <97526215+MianFaizanAmir0053@users.noreply.github.com>
2023-07-17 01:46:37 +0500

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+
 module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
   env: {
     browser: true,
     es2021: true,
